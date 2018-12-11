c522b2e9b739553820a428f610ee7f3550c4d166
Philippe <phildoreau45@gmail.com>
2018-12-11 22:40:44 +0100

    Items page

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import styled from "styled-components";
+import Item from "./Item";
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
     items {
       id
       title
+      price
       description
       image
       largeImage
@@ -14,19 +17,36 @@ const ALL_ITEMS_QUERY = gql`
   }
 `;
 
-class Items extends Component{
-  render(){
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
+
+class Items extends Component {
+  render() {
     return (
-      <div>
-        <p>Items!</p>
+      <Center>
         <Query query={ALL_ITEMS_QUERY}>
-          { ({data, error, loading}) => {
-            if(loading) return <p>Loading...</p>
-            if(error) return <p>Error: {error.message}</p>
-            return <p>I found {data.items.length} items</p>;
+          {({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error: {error.message}</p>;
+            return (
+              <ItemList>
+                {data.items.map(item => (
+                  <Item item={item} key={item.id} />
+                ))}
+              </ItemList>
+            );
           }}
         </Query>
-      </div>
+      </Center>
     );
   }
 }
